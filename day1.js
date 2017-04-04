a919bdd560ef6002659289954ce14aace961c0ae
Meg Clarke <meggyclarke@gmail.com>
2017-04-03 23:59:39 -0400

    Day1 expectation tests all passing

---
+++ day1.js
@@ -0,0 +1,27 @@
+var directions = '';
+
+
+var getFloor = function(directions) {
+  newdir = Array.from(directions);
+
+  currentFloor = 0;
+  var updateFloor = newdir.forEach(function(step) {
+    if(step == "(") {
+      currentFloor += 1;
+    }else if (step == ")") {
+      currentFloor -= 1;
+    }
+  });
+  console.log(currentFloor);
+  return currentFloor;
+};
+
+expect(getFloor('(())')).toBe(0);
+expect(getFloor('()()')).toBe(0);
+expect(getFloor('(((')).toBe(3);
+expect(getFloor('(()(()(')).toBe(3);
+expect(getFloor('))(((((')).toBe(3);
+expect(getFloor('())')).toBe(-1);
+expect(getFloor('))(')).toBe(-1);
+expect(getFloor(')))')).toBe(-3);
+expect(getFloor(')())())')).toBe(-3);
