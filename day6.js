e1eda19338a6a97a6bbfaf9b59f6dc2a7a55fefa
Meg Clarke <meggyclarke@gmail.com>
2017-04-07 13:55:51 -0400

    Completed day 6

--- day6.js
+++ day6.js
@@ -1,48 +1,17 @@
 // Lights
 
 var grid = [];
-var step1result = [];
+
 var direction = false
 
-for(step1 = 0; step1 < 5; step1++) {
+for(step1 = 0; step1 < 1000; step1++) {
   var row = [];
-  for(step2 = 0; step2 < 5; step2++) {
+  for(step2 = 0; step2 < 1000; step2++) {
     row.push(false);
 
   }
   grid.push(row);
-   step1result = grid;
 }
-
-console.log(grid);
-console.log(step1result);
-
-var visual = function(grid) { //full away
-  var string = '';
-
-  grid.forEach(function(row) {
-    var r = ''
-    row.forEach(function (light) {
-      if(light == false) {
-        r = r + "  ";
-      } else {
-        r = r + "X ";
-      }
-    })
-    string = string + r + '\n';
-  });
-  return string;
-};
-
-// var turnOn = function(grid, x1, y1, x2, y2) {
-//   for(curY = y1; curY <= y2; curY++) {
-//     for(curX = x1; curX <= x2; curX++){
-//       grid[curY][curX] = 't';
-//     }
-//   }
-//   console.log(grid);
-//   return grid;
-// }
 
 var changeLights = function (grid, x1, y1, x2, y2, direction) {
   // direction can be on off or toggle
@@ -57,14 +26,52 @@ var changeLights = function (grid, x1, y1, x2, y2, direction) {
       }
     }
   }
-  console.log(grid);
   return grid;
 }
 
-console.log(visual(grid));
+var parseCoords = function (coord) {
+  var coord1 = coord.split(',');
+  x = Number(coord1[0]);
+  y = Number(coord1[1]);
+  return {x: x, y: y}
+};
 
-grid = changeLights(grid, 2, 2, 4, 4, 'on');
-grid = changeLights(grid, 4, 4, 2, 2, 'off');
-grid = changeLights(grid, 3, 3, 5, 1, 'toggle');
+var result = function (inst) {
+  var lines = inst.split('\n');
+  console.log(lines);
+  console.log(lines[0]);
+  lines.forEach(function(line){
+    var pieces = line.split(' ');
+    console.log(pieces);
+    console.log(pieces[2]);
 
-console.log(visual(grid));
+    if (pieces.length == 4) {
+      direction = pieces[0];
+      coord1 = parseCoords(pieces[1]);
+      coord2 = parseCoords(pieces[3]);
+    }
+    else if(pieces.length == 5) {
+      direction = pieces[1];
+      console.log(direction);
+      coord1 = parseCoords(pieces[2]);
+      coord2 = parseCoords(pieces[4]);
+    }
+    grid = changeLights(grid, coord1.x, coord1.y, coord2.x, coord2.y, direction);
+    // console.log(grid);
+  });
+
+  var count = 0
+  grid.forEach(function (row){
+    row.forEach(function (spot){
+      if(spot == true){
+        count = count + 1;
+      }
+    });
+  });
+  console.log(count);
+  return count;
+};
+
+// justOne = `turn on 300,300 through 309,309
+// turn on 0,0 through 9,9`
+result(INSTRUCTIONS);
