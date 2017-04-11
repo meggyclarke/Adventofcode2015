28dec9f59ad07e130777bdd300e287a8442ddc23
Meg Clarke <meggyclarke@gmail.com>
2017-04-11 10:51:00 -0400

    Completed part 2 of day 2

--- day2.js
+++ day2.js
@@ -1,6 +1,6 @@
 //Wrapping Paper
 
-calcPaper = (function(l, w, h) {
+var calcPaper = (function(l, w, h) {
   var calc1 = (2 * l * w);    // 2*2*3 = 12
   var calc2 = 2*w*h;      // 2*3*4  = 24
   var calc3 = 2*h*l;      // 2*4*2 = 16
@@ -9,38 +9,51 @@ calcPaper = (function(l, w, h) {
   return wraptotal;
 });
 
-calcRibbon = function(w, h, l){
+var calcRibbon = function(w, h, l){
   var side1 = 2*w;
   var side2 = 2*h;
   var side3 = 2*l;
 
   var largest = Math.max(side1, side2, side3);
-  var ribOne = ((side1 + side2 + side3)-largest) + ((side1 + side2 + side3)-largest)
-//            ((2   +   3   +  4) = 9 -4 )= 5 + ((2   +   3   +  4) = 9 -4 )= 5
-//                                          = 10
-  var secondRib = w*h*l; //BOW
+  var ribOne = ((side1 + side2 + side3)-largest)
 
-  var ribbonLength = ribOne + secondRib;
+  var secondRib = w*h*l; //BOW 2*3*4 = 24
+
+  var ribbonLength = ribOne + secondRib; // 10+24 = 34
 
   console.log(ribbonLength);
   return ribbonLength
 };
 
-parseDimensions = function(dimension){
+var parseDimensions = function(dimension){
   // put the logic to turn '2x3x4' into [2, 3, 4] right here
-  var boxes = allBoxes.split('\n');
-  var boxPaper = boxDimensions.split('x');
-  console.log(boxPaper); // ["6", "3", "9"]
-  var paperNumbers=boxPaper.map(Number);
-  console.log(paperNumbers);
-  return paperNumbers // [6, 3, 9] length, width, height
+  var boxCoords = [];
+  var lines = allBoxes.split('\n');
+  console.log(lines);
+  console.log(lines[0]); // 29x13x26
+  lines.forEach(function(line){
+    var box = line.split(',');
+    console.log(box); // ['29x13x26']
+    box.forEach(function(side){
+      var boxDimensions = side.split('x');
+      console.log(boxDimensions); // ["6", "3", "9"]
+      console.log(boxDimensions[2]); // 26
+      var paperNumbers=boxDimensions.map(Number);
+      console.log(paperNumbers); [10, 9, 8]
+      boxCoords.push(paperNumbers);
+
+    //  need a new giant array i can put all my little number arrays into
+    });
+  });
+  return boxCoords;
 }
 
-function wrappingPaper() {
+var wrappingPaper = function(instructions) {
   var totalPaper = 0;
   var totalRibbon = 0;
+  var boxSize = parseDimensions(instructions);
 
-  parseDimensions.forEach(function(present) {
+  boxSize.forEach(function(present) {
     l = present[0];
     h = present[1];
     w = present[2];
@@ -57,9 +70,9 @@ function wrappingPaper() {
     ribbon: totalRibbon
   };
 }
-result = wrappingPaper();
-console.log(result.paper);
-console.log(result.ribbon);
+result = wrappingPaper(allBoxes);
+console.log(result.paper); //1,586,300 is right
+console.log(result.ribbon); // 3737498
 
 
 
